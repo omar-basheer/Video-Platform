63b98a9246009dd317a437559bd7e73d7610aea4
omar-basheer <112007297+omar-basheer@users.noreply.github.com>
2024-07-19 21:47:44 +0000

    updated host

--- video_platform/settings.py
+++ video_platform/settings.py
@@ -16,7 +16,8 @@ import base64
 import dj_database_url
 from pathlib import Path
 from google.oauth2 import service_account
-
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
