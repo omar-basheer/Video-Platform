695f7d526ab8a0a5afae526120eebbc5e2a7f4ad
omar-basheer <112007297+omar-basheer@users.noreply.github.com>
2024-07-19 20:43:12 +0000

    updated ENV VARS for postgresql

--- video_platform/settings.py
+++ video_platform/settings.py
@@ -114,11 +114,11 @@ DATABASES = {
     # }
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv('DATABASE_NAME'),
-        "USER": os.getenv('DATABASE_USER'),
-        "PASSWORD":os.getenv('DATABASE_PASS'),
-        "HOST": os.getenv('DATABASE_HOST'),
-        "PORT": os.getenv('DATABASE_PORT'),
+        "NAME": os.getenv('PGDATABASE'),
+        "USER": os.getenv('PGUSER'),
+        "PASSWORD":os.getenv('PGPASSWORD'),
+        "HOST": os.getenv('PGHOST'),
+        "PORT": os.getenv('PGPORT'),
     }
 }
 
--- vpapp/views.py
+++ vpapp/views.py
@@ -23,18 +23,18 @@ class CreateUser(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request, format=None):
-        # serializer = UserSerializer(data=request.data)
-        # if serializer.is_valid():
-        #     try:
-        #         serializer.save()
-        #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-        #     except IntegrityError as e:
-        #         return Response(
-        #             {"error": "A user with that email already exists."},
-        #             status=status.HTTP_400_BAD_REQUEST,
-        #         )
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response({"message":"Testing create user"}, status=status.HTTP_200_OK)
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            except IntegrityError as e:
+                return Response(
+                    {"error": "A user with that email already exists."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # return Response({"message":"Testing create user"}, status=status.HTTP_200_OK)
 
 
 class LoginUser(APIView):
