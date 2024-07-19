efa0bb6ad3e92080b02ffbb4a613999e7c52eb66
omar-basheer <112007297+omar-basheer@users.noreply.github.com>
2024-07-19 21:29:52 +0000

    updated host

--- video_platform/settings.py
+++ video_platform/settings.py
@@ -113,7 +113,7 @@ DATABASES = {
     #     "NAME": BASE_DIR / "db.sqlite3",
     # }
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": os.getenv('PGDATABASE'),
         "USER": os.getenv('PGUSER'),
         "PASSWORD":os.getenv('PGPASSWORD'),
